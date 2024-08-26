0fac941164790cee0d328ea065d90fb7ccf55bb3
scortuzzi <pedro.oliveira@sptech.school>
2024-08-26 20:14:28 -0300

    initial commit

---
+++ confeitech/src/main/java/br/com/confeitech/ConfeitechApplication.java
@@ -0,0 +1,13 @@
+package br.com.confeitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfeitechApplication.class, args);
+	}
+
+}
---
+++ confeitech/src/test/java/br/com/confeitech/ConfeitechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.confeitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
