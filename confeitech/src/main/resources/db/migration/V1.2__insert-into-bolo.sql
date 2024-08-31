0b7265041c03d3c71521f59b7d1186af3b286190
scortuzzi <pedro.oliveira@sptech.school>
2024-08-31 02:51:18 -0300

    config do flyway para inserção de bolos automática

---
+++ confeitech/src/main/resources/db/migration/V1.2__insert-into-bolo.sql
@@ -0,0 +1,4 @@
+insert into bolo (nome, preco, peso, descricao, contem_lactose, contem_gluten, ativo) values
+('bolo de chocolate', 12.99, 10.00, 'bolo de chocolate com granulado de brigadeiro', true, true, true),
+('bolo de cenoura', 15.39, 15.00, 'bolo de cenoura com cobertura de chocolate', false, true, true),
+('bolo de fuba', 9.59, 9.52, 'bolo de fuba para intolerantes a lactose', true, false, true);
