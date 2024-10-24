cf2cd23c5ea20b469cf1fafffb118495223a7d40
scortuzzi <pedro.oliveira@sptech.school>
2024-10-24 17:53:54 -0300

    integração com o banco na azure

--- confeitech/src/main/java/br/com/confeitech/infra/configs/FlywayConfiguration.java
+++ confeitech/src/main/java/br/com/confeitech/infra/configs/FlywayConfiguration.java
@@ -1,24 +1,24 @@
-package br.com.confeitech.infra.configs;
-
-import jakarta.annotation.PostConstruct;
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class FlywayConfiguration {
-
-    private final DataSource dataSource;
-
-    @Autowired
-    public FlywayConfiguration(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @PostConstruct
-    public void migrate() {
-        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-    }
-}
+//package br.com.confeitech.infra.configs;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.flywaydb.core.Flyway;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class FlywayConfiguration {
+//
+//    private final DataSource dataSource;
+//
+//    @Autowired
+//    public FlywayConfiguration(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @PostConstruct
+//    public void migrate() {
+//        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+//    }
+//}
--- confeitech/src/main/resources/db/migration/V1.1__insert-into-usuario.sql
+++
@@ -1,4 +0,0 @@
-insert into usuario (ativo, dt_nasc, email, nome, senha, telefone) values
-(true, '2001-09-11', 'freitas@gmail.com', 'Paulo Freitas', '$2a$10$S4t.tcFGJhIRz8KxWZLDpeG1bEov4W2pID/Q4qRCdnSQ8prjeQdeC', '11912340987'),
-(true, '1972-02-02', 'nicolas@gmail.com', 'Nicolas Laranja', '$2a$10$S4t.tcFGJhIRz8KxWZLDpeG1bEov4W2pID/Q4qRCdnSQ8prjeQdeC', '11908657974'),
-(true, '1994-01-01', 'larisso@gmail.com', 'Larisso Pereira', '$2a$10$S4t.tcFGJhIRz8KxWZLDpeG1bEov4W2pID/Q4qRCdnSQ8prjeQdeC', '11945398724');
